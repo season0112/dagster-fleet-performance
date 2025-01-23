4e1c6446f6e4103a70b2d7d9c8fb1944166c45c0
Sichen <sichen@quatt.io>
2025-01-23 12:01:26 +0800

    add .env

--- tutorial/resources.py
+++ tutorial/resources.py
@@ -4,8 +4,8 @@ from dagster import EnvVar
 # 定义 SnowflakePandasIOManager 资源
 snowflake_io_manager = SnowflakePandasIOManager(
     account="iejbhuk-quatt",
-    user="SICHEN",
-    password="h6EfZBvReBxEyuCKLdhX",
+    user=EnvVar("SNOWFLAKE_USER"),
+    password=EnvVar("SNOWFLAKE_PASSWORD"),
     warehouse="COMPUTE_WH",
     # database="DEV_SICHEN",
     # schema="IRIS",
